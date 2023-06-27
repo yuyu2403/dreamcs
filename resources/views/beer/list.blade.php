f290110560b15857cb7dfb5a469744e694cead1d
USENGROUP\j-tada <j-tada@usen-next.jp>
2023-06-27 18:08:30 +0900

    頑張ったよーん！

--- resources/views/beer/list.blade.php
+++ resources/views/beer/list.blade.php
@@ -6,79 +6,85 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
     <title>Beer Shop</title>
     <style>
         .product {
-            border: 1px solid #ccc;
-            padding: 10px;
+            border: 5px solid #5afef6;
+            padding: 60px;
             margin-bottom: 20px;
         }
         .product img {
             width: 200px;
             height: 200px;
             object-fit: cover;
-            margin-bottom: 10px;
+            margin-bottom: 50px;
         }
     </style>
 </head>
 <body>
-    <header>
-        <h1>Welcome to Beer Shop</h1>
-    </header>
 
+<div class="container">
     <div id="product-list">
         <?php
         // 商品データの配列
         $products = [
             [
-                'name' => 'Beer 1',
-                'description' => 'Description of Beer 1',
-                'price' => 9.99,
+                'name' => 'キリン 一番搾り生ビール',
+                'description' => 'アルコール成分: 5% 原材料: 麦芽（外国製造又は国内製造（5%未満））、ホップ カロリー:40kcal/100ml',
+                'price' => 280,
                 'image' => 'itibannshibori.jpg',
             ],
             [
-                'name' => 'Beer 2',
-                'description' => 'Description of Beer 2',
-                'price' => 19.99,
+                'name' => 'サッポロ 生ビール黒ラベル★',
+                'description' => 'アルコール成分: 5% 原材料:麦芽（外国製造又は国内製造（5%未満））、ホップ、米、コーン、スターチ カロリー:40kcal/100ml',
+                'price' => 245,
                 'image' => 'kuroraberu.jpg',
             ],
             [
-                'name' => 'Beer 3',
-                'description' => 'Description of Beer 3',
-                'price' => 29.99,
+                'name' => 'ヱビスビール',
+                'description' => 'アルコール成分: 5% 原材料:麦芽（外国製造又は国内製造（5%未満））、ホップ カロリー:42kcal/100ml',
+                'price' => 370,
                 'image' => 'ebisu.jpg',
             ],
             [
-                'name' => 'Beer 4',
-                'description' => 'Description of Beer 4',
-                'price' => 29.99,
+                'name' => 'ザ・プレミアム・モルツ',
+                'description' => 'アルコール成分: 5.5% 原材料:麦芽（外国製造又は国内製造）、ホップ カロリー:47kcal/100ml',
+                'price' => 420,
                 'image' => 'puremoru.jpg',
             ],
             [
-                'name' => 'Beer 5',
-                'description' => 'Description of Beer 5',
-                'price' => 29.99,
+                'name' => 'アサヒ スーパードライ',
+                'description' => 'アルコール成分: 5% 原材料:麦芽（外国製造又は国内製造（５%未満））、ホップ、米、コーン、スターチ カロリー:42kcal/100ml',
+                'price' => 280,
                 'image' => 'supadora.jpg',
             ],
             [
-                'name' => 'Beer 6',
-                'description' => 'Description of Beer 6',
-                'price' => 29.99,
+                'name' => 'クラフトビール ジュゴン',
+                'description' => 'アルコール成分: 6% 原材料:麦芽（外国製造又は国内製造（５%未満））、ホップ、米、スターチ カロリー:43kcal/100ml',
+                'price' => 770,
                 'image' => 'jyugon.jpg',
             ]
         ];
 
 
         // 商品リストを表示
-        foreach ($products as $product) {
-            echo "<div class='product'>";
-            echo "<img src='images/{$product['image']}' alt='{$product['name']}' />";
-            echo "<h3>{$product['name']}</h3>";
-            echo "<p>{$product['description']}</p>";
-            echo "<p>Price: {$product['price']}</p>";
-            echo "</div>";
-        }
+            foreach ($products as $product) {
+                echo "<div class='product'>";
+                echo "<img src='images/{$product['image']}' alt='{$product['name']}' />";
+                echo "<h3>{$product['name']}</h3>";
+                echo "<p>{$product['description']}</p>";
+                echo "<p>Price: &#165;{$product['price']}</p>";  // 日本円表記を追加
+                echo "</div>";
+
+}
         ?>
     </div>
+</div>
+
 </body>
 </html>
