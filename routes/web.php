1d0c0b324a8c851064e4f9a55c0263565244d80e
USENGROUP\j-tada <j-tada@usen-next.jp>
2023-06-27 14:59:12 +0900

    追加したよ

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/rena', function () {
+    return view('rena');
+});
