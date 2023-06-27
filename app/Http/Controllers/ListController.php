a32d9b14289773f8d870bcb07f459ed7e628fb33
USENGROUP\sh-kubota <sh-kubota@usen-next.jp>
2023-06-27 17:19:27 +0900

    list確認してね

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+App\Models\;
 
 
 
 class ListController extends Controller
 {
-    //
+    public function list(Request $request){
+        $keyword = $request->input('keyword');
+        $query = List::query();
+        if ($request->has('check')) {
+            /* Contact モデルのオブジェクトを作成 */
+            $id = $request->id;
+            $new_contact = List::find($id);
+            /* リクエストで渡された値を設定する */
+            $new_contact->check = $request->check;
+            /* データベースに保存 */
+            $new_contact->save();
+            /* 完了画面を表示する */
+            return redirect('/beer/list');
+        }
+        if (!empty($keyword)) {
+            $query->where('name', 'LIKE', "%{$keyword}%")
+                ->orWhere('tel', 'LIKE', "%{$keyword}%")
+                ->orWhere('address', 'LIKE', "%{$keyword}%")
+                ->orWhere('point', 'LIKE', "%{$keyword}%");
+        }
+        $contacts = $query->get();
+        return view('beer.list', compact('contacts', 'keyword'));
+    }
 }
