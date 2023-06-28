50b2b9e2dfdc2fabfa03be2ec664fca2bf8905eb
chanarry <a-nozawa@usen-next.jp>
2023-06-28 16:48:14 +0900

    カートつくってみた

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Item;
 use App\Models\Cart;
@@ -13,4 +14,15 @@ use App\Models\User;
 
 class CartController extends Controller
 {
+    public function cart()
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $items = Item::all();
+            $cart_items = Cart::where('user_id', $user->id)->get();
+            return view('beer.cart', compact('user', 'items', 'cart_items'));
+        } else {
+            return view('auth.login');
+        }
+    }
 }
