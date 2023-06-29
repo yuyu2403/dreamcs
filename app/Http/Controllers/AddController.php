1667106501a38e2c330a71a257614bd530fc7399
chanarry <a-nozawa@usen-next.jp>
2023-06-29 16:36:59 +0900

    がぞうだよーーーー

--- app/Http/Controllers/AddController.php
+++ app/Http/Controllers/AddController.php
@@ -11,11 +11,11 @@ class AddController extends Controller
 {
 
     public function add()
-
     {
         $all_categories = Category::all();
         return view('beer.add', compact('all_categories'));
     }
+
 
     public function addconfirm(Request $request)
     {
@@ -32,37 +32,63 @@ class AddController extends Controller
             'stock'  => ['required'],
 
             'category_id'  => ['required'],
+        ]);
 
+        $request->validate([
             'image' => 'required|max:1024|mimes:jpg,jpeg,png,gif'
         ]);
 
+        $imageName = $request->file('image')->getClientOriginalName();
+        $extension = $request->file('image')->getClientOriginalExtension();
 
-        return view('beer.addconfirm', compact('request', 'category'));
+        // 新しいファイル名を生成（形式：元のファイル名_ランダムの英数字.拡張子）
+        $newImageName = pathinfo($imageName, PATHINFO_FILENAME) . "_" . uniqid() . "." . $extension;
+        $request->file('image')->move(public_path() . "/img/tmp", $newImageName);
+        $image = "/img/tmp/" . $newImageName;
+        // $file_path = $request->file('image')->store('images', 'public');
+
+        return view('beer.addconfirm', compact('request', 'category', 'image', 'newImageName'));
+        // return view('beer.addconfirm', compact('request', 'category', 'file_path', 'image', 'newImageName'));
     }
 
-    public function send_or_back(Request $request)
+    public function send_or_back(Request $request,)
     {
         if ($request->has('back')) {
             return redirect('/add')->withInput();
         }
 
         if ($request->has('send')) {
-            $file_path = $request->image->store('images', 'public');
-            $new_item = new Item();
 
+            $new_item = new Item();
             $new_item->name = $request->name;
             $new_item->detail = $request->detail;
             $new_item->price = $request->price;
             $new_item->stock = $request->stock;
             $new_item->category_id = $request->category_id;
-            $new_item->filename = $request->image->getClientOriginalName();
-            $new_item->filepath = $file_path;
-
+            $new_item->filepath = $request->image;
+            // $new_item->filename = $request->file('image')->getClientOriginalName();
+            // $new_item->filepath = $request->file('image')->store('images', 'public');
             $new_item->save();
 
-            return redirect('/list');
-            // return view('beer.list');
 
+
+
+            // レコードを挿入したときのIDを取得
+            $lastInsertedId = $new_item->id;
+
+            // ディレクトリを作成
+            if (!file_exists(public_path() . "/img/" . $lastInsertedId)) {
+                mkdir(public_path() . "/img/" . $lastInsertedId, 0777);
+            }
+
+            // 一時保存から本番の格納場所へ移動
+            rename(public_path() . "/img/tmp/" . $request->image, public_path() . "/img/" . $lastInsertedId . "/" . $request->image);
+
+            // 一時保存の画像を削除
+            // \File::cleanDirectory(public_path() . "/img/tmp");
+
+
+            return redirect('/list');
         }
     }
 }
