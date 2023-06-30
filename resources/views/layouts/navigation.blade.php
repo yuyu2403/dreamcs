8c333e1dfd316dd3eb069c237f18013aec7b9642
chanarry <a-nozawa@usen-next.jp>
2023-06-30 10:26:27 +0900

    ダッシュボードに商品一覧ボタン追加　じんじん浅草みんなでいくよ～～～

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('list')" :active="request()->routeIs('list')">
+                        {{ __('商品一覧') }}
+                    </x-nav-link>
+
                 </div>
             </div>
 
